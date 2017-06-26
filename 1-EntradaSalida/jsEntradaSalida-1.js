784e1a863d70f9d61c21e9e101541bde397e7266
Gonza97 <chateas.gon.gonzy@hotmail.com>
2017-06-26 19:47:33 -0300

    Ejercicio echo de nuevo

--- 1-EntradaSalida/jsEntradaSalida-1.js
+++ 1-EntradaSalida/jsEntradaSalida-1.js
@@ -1,8 +1,11 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostar()
 {
+    var nombre;
+    nombre = alert("Esto funciona de maravilla");
     
     
+ 
 
 }
 
