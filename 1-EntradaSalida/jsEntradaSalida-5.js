558c1ef6e44ec2a172e5c8a3b32afa351e18bb27
Gonza97 <chateas.gon.gonzy@hotmail.com>
2017-06-27 20:33:51 -0300

    Trabajo 6 y correcciones

--- 1-EntradaSalida/jsEntradaSalida-5.js
+++ 1-EntradaSalida/jsEntradaSalida-5.js
@@ -1,7 +1,27 @@
 /*Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados 
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function Mostar()
-{	
+{
+    var nombre  = document.getElementById("elNombre").value;
+    var edad = document.getElementById("laEdad").value;
+    var msj1 = ("Usted se llama");
+    var msj2 = ("y tiene");
+    var msj3 = ("años");
+    var msjt = msj1.concat(nombre) + " " + msj2.concat(edad) + " " + ("años");
+    alert(msjt);
+
+    
+   
+   
+
+
+    
+    
+
+
+    
+    
+
 	
 }
 
--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,11 @@ transformarlos a enteros (parseInt) y Sumarlos.
 Mostar el resulto por medio de "ALERT"*/
 function sumar()
 {
+    var n1 = document.getElementById("numeroUno").value;
+    var n2 = document.getElementById("numeroDos").value;
+    var res = parseInt(n1) + parseInt(n2);
+    alert(res);
+    
 
 }
 
